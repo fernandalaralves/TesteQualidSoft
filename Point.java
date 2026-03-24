b80db3bf062ee9c372f0c322aec2b68d23c745fd
Fernanda Lara Alves <fernandalaralves@gmail.com>
2026-03-24 15:54:48 -0300

    init

---
+++ Point.java
@@ -0,0 +1,47 @@
+public class Point
+{
+        private double X;
+        private double Y;
+
+        public Point (double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public void setX( double X)
+        {
+            this.X = X;
+        }
+
+        public void setY (double Y)
+        {
+            this.Y = Y;
+        }
+
+        public double getX()
+        {
+            return X;
+        }
+        public double getY()
+        {
+            return Y;
+        }
+
+        public boolean equalTo( Point A)
+        {
+            if ( X == A.getX() && Y == A.getY())
+                return true;
+            else
+                return false;
+        }
+
+        public double distanceTo (Point A)
+        {
+            return Math.sqrt( Math.pow(X - A.getX(), 2.0) + Math.pow( Y - A.getY(), 2.0));
+        }
+        public String toString()
+        {
+            return String.format("(%.2f;%.2f)", X,Y);
+        }
+}
