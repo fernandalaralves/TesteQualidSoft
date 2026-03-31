6c9adb1c8ecb3d8db157c77725757c82c9b5d12e
Fernanda Lara Alves <fernandalaralves@gmail.com>
2026-03-31 11:10:08 -0300

    Correção do cálculo do SalarioAnual

--- Questao1_Employee/EmployeeTest.java
+++ Questao1_Employee/EmployeeTest.java
@@ -4,19 +4,17 @@ public class EmployeeTest {
 
     public static void main(String[] args) {
         Employee emp1 = new Employee("Maria", "Silva", 1500.00);
-        Employee emp2 = new Employee("Clara", "Souza", -6000.00);
-
+        Employee emp2 = new Employee("Clara", "Souza", 6000.00);
 
         System.out.println(" Salários Anuais Iniciais ");
-        System.out.printf("%s %s: R$ %.2f%n", emp1.getNome(), emp1.getUltimoNome(), emp1.getSalarioMensal());
-        System.out.printf("%s %s: R$ %.2f%n", emp2.getNome(), emp2.getUltimoNome(), emp2.getSalarioMensal());
+        System.out.printf("%s %s: R$ %.2f%n", emp1.getNome(), emp1.getUltimoNome(), emp1.getSalarioMensal() * 12);
+        System.out.printf("%s %s: R$ %.2f%n", emp2.getNome(), emp2.getUltimoNome(), emp2.getSalarioMensal() * 12);
 
-        // Aumento de 10%
         emp1.setSalarioMensal(emp1.getSalarioMensal() * 1.10);
         emp2.setSalarioMensal(emp2.getSalarioMensal() * 1.10);
 
-        System.out.println("\n=== Salários Anuais Após Aumento de 10% ===");
-        System.out.printf("%s %s: R$ %.2f%n", emp1.getNome(), emp1.getUltimoNome(), emp1.getSalarioAnual());
-        System.out.printf("%s %s: R$ %.2f%n", emp2.getNome(), emp2.getUltimoNome(), emp2.getSalarioAnual());
+        System.out.println(" Salários Anuais Após Aumento de 10% ");
+        System.out.printf("%s %s: R$ %.2f%n", emp1.getNome(), emp1.getUltimoNome(), emp1.getSalarioMensal() * 12);
+        System.out.printf("%s %s: R$ %.2f%n", emp2.getNome(), emp2.getUltimoNome(), emp2.getSalarioMensal() * 12);
     }
 }
