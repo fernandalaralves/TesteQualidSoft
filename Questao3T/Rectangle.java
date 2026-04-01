f5e281b3c6f37bb1f467d87fa37855f0921b648e
Fernanda Lara Alves <fernandalaralves@gmail.com>
2026-04-01 10:00:09 -0300

    Retangle herda de Quadrilateral

--- Questao3T/Rectangle.java
+++ Questao3T/Rectangle.java
@@ -1,12 +1,12 @@
 package Questao3T;
 
-public class Rectangle extends Parallelogram{
+public class Rectangle extends Quadrilateral{
     private double Largura;
     private double Comprimento;
 
     public Rectangle(Point A, Point B, Point C, Point D, double Largura, double Comprimento) {
 
-        super(A, B, C, D, Largura, Comprimento);
+        super(A, B, C, D);
         this.Largura = Largura;
         this.Comprimento = Comprimento;
     }
