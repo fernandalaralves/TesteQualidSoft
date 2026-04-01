379e813f5b4a7d9ff7cefee38c30afa83b4df0aa
Fernanda Lara Alves <fernandalaralves@gmail.com>
2026-04-01 09:58:47 -0300

    Square herda de Quadrilateral

--- Questao3T/Square.java
+++ Questao3T/Square.java
@@ -1,10 +1,10 @@
 package Questao3T;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
     private double Lado;
 
     public Square (Point A, Point B, Point C, Point D, double Lado){
-        super( A, B, C, D, Lado, Lado);
+        super( A, B, C, D);
 
         this.Lado = Lado;
     }
